bde715ddd4df02820579aa72e942bca9b3c8f147
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2025-01-14 13:37:33 +0530

    feat: implementing dashboard with zod validation

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,7 +1,63 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { useUser } from "@clerk/nextjs";
 import React from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { usernameSchema } from "@/app/lib/validators";
+import { useEffect } from "react";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const { isLoaded, user } = useUser();
+  console.log(user);
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(usernameSchema),
+  });
+
+  // by default username
+  useEffect(() => {
+    setValue("username", user?.username);
+  }, [isLoaded]);
+
+  const onSubmit = async (data) => {};
+  return (
+    <div className="space-y-8">
+      <Card>
+        <CardHeader>
+          <CardTitle>Welcome, {`${user?.firstName}`}</CardTitle>
+        </CardHeader>
+        {/* Latest Updates */}
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>Your Unique Link</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+            <div>
+              <div className="flex items-center gap-2">
+                <span>{window?.location.origin}/</span>
+                <Input {...register("username")} placeholder="username" />
+              </div>
+              {errors.username && (
+                <p className="text-red-500 text-sm mt-1">
+                  {errors.username.message}
+                </p>
+              )}
+            </div>
+            <Button type="submit">Update username</Button>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default Dashboard;
