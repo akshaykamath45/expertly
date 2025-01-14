0e26f2e83c2f6d968caeb4537979aa9653f5a777
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2025-01-15 02:44:54 +0530

    style: custom user page

---
+++ app/[username]/page.jsx
@@ -0,0 +1,46 @@
+import { getUserByUsername } from "@/actions/users";
+import EventCard from "@/components/event-card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { notFound } from "next/navigation";
+import React from "react";
+
+const UserPage = async ({ params }) => {
+  // console.log(params.username);
+  const user = await getUserByUsername(params?.username);
+  if (!user) {
+    notFound();
+  }
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <div className="flex flex-col items-center mb-10 gap-4">
+        <Avatar>
+          <AvatarImage src={user.imageUrl} alt={user.name} />
+          <AvatarFallback >{user.name.charAt(0)}</AvatarFallback>
+        </Avatar>
+        <h1 className="text-3xl font-bold mb-2">{user.name}</h1>
+        <p className="text-gray-600 text-center">
+          Welcome to my scheduling page. Please select an event below to have
+          call/chat with me
+        </p>
+      </div>
+      {user.events.length === 0 ? (
+        <p className="text-gray-600 text-center">No public events available</p>
+      ) : (
+        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+          {user.events.map((event) => {
+            return (
+              <EventCard
+                key={event.id}
+                event={event}
+                username={params?.username}
+                isPublic
+              />
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UserPage;
