624687385144ffdbd17e2173a58ce34f4c1a8656
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2025-01-15 19:53:42 +0530

    feat: implementing events page and action

---
+++ actions/meetings.js
@@ -0,0 +1,42 @@
+"use server";
+
+import { db } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+
+export async function getUserMeetings(type = "upcoming") {
+  const { userId } = await auth();
+
+  if (!userId) {
+    throw new Error("Unauthorized: No user ID found.");
+  }
+  // finding user in db
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+  });
+  if (!user) {
+    throw new Error("User not found");
+  }
+  const now = new Date();
+  const meetings = await db.booking.findMany({
+    where: {
+      userId: user.id,
+      startTime: type === "upcoming" ? { gte: now } : { lt: now },
+    },
+    include: {
+      event: {
+        include: {
+          user: {
+            select: {
+              name: true,
+              email: true,
+            },
+          },
+        },
+      },
+    },
+    orderBy: {
+      startTime: type === "upcoming" ? "asc" : "desc",
+    },
+  });
+  return meetings;
+}
---
+++ app/(main)/meetings/page.jsx
@@ -0,0 +1,44 @@
+import { getUserMeetings } from "@/actions/meetings";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import React, { Suspense } from "react";
+import MeetingList from "./_components/meeting-list";
+
+export const metadata = {
+  title: "Your Meetings | Expertly",
+  description: "View and manage your upcoming and past meetings",
+};
+
+const MeetingPage = () => {
+  return (
+    <div>
+      <Tabs defaultValue="upcoming" className="w-[400px]">
+        <TabsList>
+          <TabsTrigger value="upcoming">Upcoming</TabsTrigger>
+          <TabsTrigger value="past">Past</TabsTrigger>
+        </TabsList>
+        <TabsContent value="upcoming">
+          <Suspense fallback={<div>Loading upcoming meetings...</div>}>
+            <UpcomingMeetings />
+          </Suspense>
+        </TabsContent>
+        <TabsContent value="past">
+          <Suspense fallback={<div>Loading past meetings...</div>}>
+            <PastMeetings />
+          </Suspense>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+};
+
+async function UpcomingMeetings() {
+  const meetings = await getUserMeetings("upcoming");
+  return <MeetingList meetings={meetings} type={"upcoming"} />;
+}
+
+async function PastMeetings() {
+  const meetings = await getUserMeetings("past");
+  return <MeetingList meetings={meetings} type={"past"} />;
+}
+
+export default MeetingPage;
